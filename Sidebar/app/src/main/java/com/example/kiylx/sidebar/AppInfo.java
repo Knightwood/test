38cbcc281a46f97cc72ac00379f4c2033562b0d8
Knightwood <33772264+Knightwood@users.noreply.github.com>
2018-08-21 10:08:36 +0800

    2018/8/21

---
+++ Sidebar/app/src/main/java/com/example/kiylx/sidebar/AppInfo.java
@@ -0,0 +1,32 @@
+package com.example.kiylx.sidebar;
+
+import android.graphics.drawable.Drawable;
+
+public class AppInfo {
+    String appName;
+    Drawable drawable;
+    public AppInfo(){}
+    public AppInfo(String appName){
+        this.appName = appName;
+    }
+    public AppInfo(String appName, Drawable drawable){
+        this.appName = appName;
+        this.drawable =drawable;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public Drawable getDrawable() {
+        return drawable;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        this.drawable = drawable;
+    }
+}
