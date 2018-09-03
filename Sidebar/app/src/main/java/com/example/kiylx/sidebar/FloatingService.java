6d179a72db6ba3b52b4be5d5b074d85d3dc25bbe
Knightwood <33772264+Knightwood@users.noreply.github.com>
2018-09-03 14:02:23 +0800

    2018,9,3

--- Sidebar/app/src/main/java/com/example/kiylx/sidebar/FloatingService.java
+++ Sidebar/app/src/main/java/com/example/kiylx/sidebar/FloatingService.java
@@ -1,49 +1,97 @@
 package com.example.kiylx.sidebar;
 
+import android.annotation.TargetApi;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.provider.Settings;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class FloatingService extends Service {
+    WindowManager windowManager;
+    WindowManager.LayoutParams params;
+        //Button buttonOn;
     public FloatingService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
+        showFloatingWindow();
         return super.onStartCommand(intent,flags,startId);
     }
-    private void showFloatingWindow(){
-        if(Settings.canDrawOverlays(this)){
-            WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-            Button button = new Button(getApplicationContext());
-            button.setText("Floating Window");
-            button.setBackgroundColor(Color.BLUE);
 
-            WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
-            if(Build.VERSION.SDK_INI >= Build.VERSION_CODES.0){
-                layoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+
+
+    private void showFloatingWindow(){
+        if(Settings. canDrawOverlays(this)){
+            windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+            Button button = new Button(getApplicationContext());
+            button.setText("@string/Window");
+            button.setBackgroundColor(Color.BLUE);
+            button.setWidth(100);
+            button.setHeight(100);
+            button.setId(R.id.button_11);
+            //buttonOn = (Button)View.findViewById(R.id.button_11);
+            Log.d("mainActivity","on_2");
+            //windowManager.removeView(button);
+
+            params = new WindowManager.LayoutParams();
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+                Log.d("mainActivity","on_1");
             }else {
-                layoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+                params.type = WindowManager.LayoutParams.TYPE_PHONE;
             }
-            layoutParams.format = PixelFormat.RGBA_8888;
-            layoutParams.width = 500;
-            layoutParams.height = 100;
-            layoutParams.x = 300;
-            layoutParams.y = 300;
-            windowManager.addView(button, layoutParams);
+            params.gravity = Gravity.LEFT | Gravity.TOP;
+            params.format = PixelFormat.RGBA_8888;
+            params.width = 500;
+            params.height = 100;
+            params.x = 300;
+            params.y = 300;
+            windowManager.addView(button,params);
+            button.setOnClickListener(new View.OnClickListener() {
+                long[] hints = new long[2];
+                @Override
+                public void onClick(View view) {
+
+                    System.arraycopy(hints,1,hints,0,hints.length -1);
+                    hints[hints.length -1]=SystemClock.uptimeMillis();
+                    if(SystemClock.uptimeMillis()-hints[0]>=2000){
+                        Toast.makeText(FloatingService.this,"连续点击两次以退出", Toast.LENGTH_SHORT).show();
+                    }else{
+                        stopSelf();
+                        Log.d("mainActivity","hb");
+                    }
+
+
+                }
+            });
+            Log.d("mainActivity","on");
         }
         }
+    @Override
+    public void onDestroy(){
+        /*if(button !=null){
+            windowManager.removeView(button);
+        }*/
+        super.onDestroy();
+        Log.d("mainActivity","jj");
+
+    }
 
     }
 
--- Sidebar/app/src/main/java/com/example/kiylx/sidebar/mainActivity.java
+++ Sidebar/app/src/main/java/com/example/kiylx/sidebar/mainActivity.java
@@ -5,7 +5,9 @@ import android.net.Uri;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class mainActivity extends AppCompatActivity {
@@ -17,20 +19,24 @@ public class mainActivity extends AppCompatActivity {
     }
     public void startFloatingService(View view){
         if(!Settings.canDrawOverlays(this)){
-            Toast.makeText(this,"请授予悬浮窗权限",Toast.LENGTH_SHORT);
+            Toast.makeText(this,"请授予悬浮窗权限",Toast.LENGTH_SHORT).show();
             startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:"+getPackageName())), 0);
             }else{
             startService(new Intent(mainActivity.this,FloatingService.class));
+            Log.d("mainActivity","on_5");
         }
     }
 
     @Override
-    protected void onActivityReasult(int requestCode, int resultCode,Intent data){
-        if(requestCode == 0){
-            Toast.makeText(this,"授权失败",Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(mainActivity.this,"授权成功",Toast.LENGTH_SHORT).show();
-            startService(new Intent(mainActivity.this, FloatingService.class));
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+                Log.d("mainActivity","on_3");
+            } else {
+                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                startService(new Intent(mainActivity.this, FloatingService.class));
+                Log.d("mainActivity","on_4");
+            }
         }
-    }
-}
+    }}
