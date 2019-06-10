92027adb935718b1a6187df7751b0d920e247406
Knightwood <33772264+Knightwood@users.noreply.github.com>
2019-06-10 11:16:00 +0800

    9完成

--- CriminalIntent/app/src/main/java/android/bignerdranch/com/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/android/bignerdranch/com/CrimeListFragment.java
@@ -6,12 +6,20 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
 
     @Nullable
     @Override
@@ -19,6 +27,83 @@ public class CrimeListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_crime_list,container,false);
         mCrimeRecyclerView=view.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
         return view;
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+        private List<Crime> mCrimes;
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @NonNull
+        @Override
+        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+            LayoutInflater layoutInflater=LayoutInflater.from(getActivity());
+            /*View linearLayout=layoutInflater.inflate(R.layout.list_item_crime,viewGroup,false);
+            return new CrimeHolder(linearLayout);
+            这是我自己写的方式，直接改变viewholder的形参，不按照书上的写也是可以的。这跟以前写的是一样的原理。
+        */
+            return new CrimeHolder(layoutInflater, viewGroup);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CrimeHolder crimeHolder, int position) {
+
+            Crime crime = mCrimes.get(position);
+            crimeHolder.bind(crime);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        private Crime mCrime;
+
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedImageView.setVisibility(crime.isSolved()? View.VISIBLE:View.INVISIBLE);
+        }
+
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private ImageView mSolvedImageView;
+
+        /*public CrimeHolder(View il) {//自己写的，把形参改为View il
+            super(il);
+
+            mTitleTextView = (TextView) il.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) il.findViewById(R.id.crime_date);
+        }*/
+
+            public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+                super(inflater.inflate(R.layout.list_item_crime, parent, false));
+                mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+                mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+                mSolvedImageView = itemView.findViewById(R.id.crime_solved);
+                itemView.setOnClickListener(this);
+                //不是很理解itemview，难道是默认的一种语法糖？
+            }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(),
+                    mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
