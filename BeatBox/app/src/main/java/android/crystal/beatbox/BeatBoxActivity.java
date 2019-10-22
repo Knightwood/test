f88eb095b3be1e78b61a6a93fca736a36c9ce024
Knightwood <kiylx@outlook.com>
2019-10-22 11:43:36 +0800

    2

--- BeatBox/app/src/main/java/android/crystal/beatbox/BeatBoxActivity.java
+++ BeatBox/app/src/main/java/android/crystal/beatbox/BeatBoxActivity.java
@@ -1,14 +1,32 @@
 package android.crystal.beatbox;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SingleFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beat_box);
+        setContentView(R.layout.fragment_beat_box);
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+
 }
