b33546762d3e52beeed023b16df2c6c56a8a02bd
Knightwood <kiylx@outlook.com>
2019-10-22 10:48:39 +0800

    fixed untracked files

---
+++ BeatBox/app/src/main/java/android/crystal/beatbox/BeatBoxActivity.java
@@ -0,0 +1,14 @@
+package android.crystal.beatbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beat_box);
+    }
+}
