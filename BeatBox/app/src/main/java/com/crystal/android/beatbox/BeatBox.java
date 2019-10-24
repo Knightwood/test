3e02dc75d75040d4f0481cc4d9603eb5c94aca9a
Knightwood <kiylx@outlook.com>
2019-10-24 23:08:53 +0800

    完成第20章

--- BeatBox/app/src/main/java/com/crystal/android/beatbox/BeatBox.java
+++ BeatBox/app/src/main/java/com/crystal/android/beatbox/BeatBox.java
@@ -1,7 +1,13 @@
 package com.crystal.android.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.util.Log;
 
 import java.io.IOException;
@@ -10,12 +16,32 @@ import java.util.List;
 
 public class BeatBox {
     private static final String TAG = "BeatBox";
+
     private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUNDS = 5;
+
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            AudioAttributes audioAttributes = null;
+            /*
+             * setUsage用来设置用途(比如是游戏还是媒体),
+             * setContentType用来设置内容类型(比如是视频还是音乐)*/
+            audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build();
+            mSoundPool = new SoundPool.Builder()
+                    .setMaxStreams(MAX_SOUNDS)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        } else {
+            mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+        }
         loadSounds();
     }
 
@@ -33,6 +59,11 @@ public class BeatBox {
             Sound sound = new Sound(assetPath);
             mSounds.add(sound);
         }
+    }
+    private void load(Sound sound) throws IOException{
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd,1);
+        sound.setSoundId(soundId);
     }
 
     public List<Sound> getSounds() {
