bbaa796dfb450853aa042ec01f1e908f9f3048f7
Knightwood <kiylx@outlook.com>
2019-11-08 23:55:12 +0800

    +引入新的bug +重构代码 +时间获取一周时数据不对，其他时间的获取开始时间到不了1号 +用接口的方式让历史记录和收藏的功能解耦 +开始添加下载功能

---
+++ Ti/app/src/main/java/com/example/kiylx/ti/Activitys/DownloadActivity.java
@@ -0,0 +1,16 @@
+package com.example.kiylx.ti.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kiylx.ti.R;
+
+public class DownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+    }
+}
