b9b3883d5d50ef5e094dfd9479d40cc8fa595471
Knightwood <kiylx@outlook.com>
2019-11-09 14:20:30 +0800

    +引入新的bug +重构代码 +时间获取一周时数据不对，其他时间的获取开始时间到不了1号 +用接口的方式让历史记录和收藏的功能解耦 +开始添加下载功能

--- Ti/app/src/main/java/com/example/kiylx/ti/DownloadCore/DownloadTask.java
+++ Ti/app/src/main/java/com/example/kiylx/ti/DownloadCore/DownloadTask.java
@@ -1,10 +1,109 @@
 package com.example.kiylx.ti.DownloadCore;
 
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.webkit.DownloadListener;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class DownloadTask extends AsyncTask<String, Integer, Integer> {
+
+    public static final int TYPE_SUCCESS = 0;
+    public static final int TYPE_FAILED = 1;
+    public static final int TYPE_PAUSED = 2;
+    public static final int TYPE_CANCELED = 3;
+
+    private DownloadListener listener;
+    private boolean isCanceled = false;
+    private boolean isPaused = false;
+    private int lastProgress;
+
+    public DownloadTask(DownloadListener listener) {
+        this.listener=listener;
+    }
+
     @Override
     protected Integer doInBackground(String... strings) {
-        return null;
+        InputStream in=null;
+        RandomAccessFile savedFile=null;
+        File file=null;
+        try {
+            long downloadedLength=0;
+            String downloadUrl=strings[0];
+            String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
+            String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();//存储下载的路径
+
+            file=new File(directory+fileName);
+
+            //如果文件存在，说明已经下载或是刚开始，所以获取文件长度作为已下载的文件长度
+            if (file.exists()){
+                downloadedLength=file.length();
+            }
+
+            //要下载的文件的长度
+            long contentLength=getContentLength(downloadUrl);
+
+            //判断文件是否下载成功
+            if (contentLength==0){
+                return TYPE_FAILED;
+            }else if (contentLength==downloadedLength){
+                return TYPE_SUCCESS;
+            }
+
+            //利用添加Range:bytes=512000- 请求头实现断点传续
+            OkHttpClient client =new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(strings[0])
+                    .addHeader("RANGR","bytes="+downloadedLength+"-")
+                    .build();
+            //使用同步请求取得回应
+            Response response=client.newCall(request).execute();
+
+            //拿到服务器传进来的数据，存入新建的文件
+            if (response!=null){
+                //输入流
+                in=response.body().byteStream();
+
+                //创建一个随机输入文件，可以写入或读取文件。
+                savedFile=new RandomAccessFile(file,"rw");
+                savedFile.seek(downloadedLength);// 跳过已下载的字节
+
+                byte[] b =new byte[1024];
+
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+        return TYPE_FAILED;
+
     }
+
+    private long getContentLength(String downloadUrl) {
+    }
+
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        super.onPostExecute(integer);
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+
 }
