354ac06a80b5327fd841c72cca0ccb0db9c67abd
Knightwood <kiylx@outlook.com>
2019-11-20 09:31:54 +0800

    +开发下载功能中

---
+++ Ti/app/src/main/java/com/example/kiylx/ti/INTERFACE/DOWNLOAD_TASK_FUN.java
@@ -0,0 +1,18 @@
+package com.example.kiylx.ti.INTERFACE;
+
+import com.example.kiylx.ti.Corebase.DownloadInfo;
+
+public interface DOWNLOAD_TASK_FUN {
+
+    /**
+     * @return 暂停下载的一些操作
+     */
+     boolean pausedDownload(DownloadInfo info);
+
+    /**
+     * @return 取消下载的一些操作
+     */
+    boolean canceledDownload(DownloadInfo info);
+
+    boolean downloadSucess(DownloadInfo info);
+}
