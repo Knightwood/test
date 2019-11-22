8343d27a94d13435b137720f169ee0fef2412760
Knightwood <kiylx@outlook.com>
2019-11-23 00:01:02 +0800

    +开发下载功能中 +界面不好写

--- Ti/app/src/main/java/com/example/kiylx/ti/Fragments/DownloadWindow.java
+++ Ti/app/src/main/java/com/example/kiylx/ti/Fragments/DownloadWindow.java
@@ -5,6 +5,8 @@ import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
@@ -26,11 +28,14 @@ public class DownloadWindow extends DialogFragment {
     private View downloadWindow;
     private DownloadInfo mDownloadInfo;
 
-    private DownloadWindow(DownloadInfo info){
-        this.mDownloadInfo=info;
+    private TextView fileNameView;
+    private TextView fileUrlView;
+
+    private DownloadWindow(DownloadInfo info) {
+        this.mDownloadInfo = info;
     }
 
-    public DownloadWindow getInstance(DownloadInfo info){
+    public DownloadWindow getInstance(DownloadInfo info) {
 
         return new DownloadWindow(info);
     }
@@ -38,8 +43,8 @@ public class DownloadWindow extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder=new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
-        downloadWindow=getLayoutInflater().inflate(R.layout.downloadwindow,null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
+
         builder.setView(downloadWindow)
                 .setPositiveButton("确认", new DialogInterface.OnClickListener() {
                     @Override
@@ -55,13 +60,37 @@ public class DownloadWindow extends DialogFragment {
                 });
 
         //设置界面信息
-        setInfo();
+        setInfo(null);
+
         return builder.create();
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        downloadWindow = getLayoutInflater().inflate(R.layout.downloadwindow, null);
+        fileNameView = this.downloadWindow.findViewById(R.id.filename_d);
+        fileUrlView = this.downloadWindow.findViewById(R.id.fileurl_d);
+
+        fileNameView.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                fileNameView.removeTextChangedListener(this);
+                setInfo(s.toString());
+                fileNameView.addTextChangedListener(this);
+            }
+        });
     }
 
     @Override
@@ -87,15 +116,13 @@ public class DownloadWindow extends DialogFragment {
         }
     }
 
-    private void setInfo(){
-        if (mDownloadInfo==null){
+    private void setInfo(String fileName) {
+
+        if (mDownloadInfo == null) {
             //处理错误
         }
+        fileNameView.setText(fileName == null ? fileName : mDownloadInfo.getFileName());
 
-        TextView title=this.downloadWindow.findViewById(R.id.downloadTtitle);
-        TextView url=this.downloadWindow.findViewById(R.id.downloadUrl);
-
-        title.setText(mDownloadInfo.getFileName());
-        url.setText(mDownloadInfo.getUrl());
+        fileUrlView.setText(mDownloadInfo.getUrl());
     }
 }
--- Ti/app/src/main/java/com/example/kiylx/ti/activitys/DownloadActivity.java
+++ Ti/app/src/main/java/com/example/kiylx/ti/activitys/DownloadActivity.java
@@ -1,6 +1,8 @@
 package com.example.kiylx.ti.activitys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.ComponentName;
 import android.content.Intent;
@@ -9,10 +11,19 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.example.kiylx.ti.Corebase.DownloadInfo;
 import com.example.kiylx.ti.Discard.CustomDownloadService;
 import com.example.kiylx.ti.R;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DownloadActivity extends AppCompatActivity {
 
@@ -21,7 +32,7 @@ public class DownloadActivity extends AppCompatActivity {
     private ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            downloadBinder=(CustomDownloadService.DownloadBinder) service;
+            downloadBinder = (CustomDownloadService.DownloadBinder) service;
         }
 
         @Override
@@ -36,13 +47,82 @@ public class DownloadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_download);
 
 //绑定服务
-        Intent intent = new Intent(DownloadActivity.this,CustomDownloadService.class);
+        Intent intent = new Intent(DownloadActivity.this, CustomDownloadService.class);
         startService(intent);
-        bindService(intent,connection,BIND_AUTO_CREATE);
+        bindService(intent, connection, BIND_AUTO_CREATE);
 
 
     }
 
+    private void updateUI() {
+
+    }
+
+    private class DownloadViewAdapter extends RecyclerView.Adapter<DownloadViewHolder> {
+        List<DownloadInfo> infos;
+
+        public DownloadViewAdapter() {
+            infos = new ArrayList<>();
+        }
+
+        /**
+         * @param list 所有的下列信息
+         *             赋值下载信息的列表，准备构建recyclerview视图
+         */
+        public void setLists(List<DownloadInfo> list) {
+            this.infos = list;
+        }
+
+        @NonNull
+        @Override
+        public DownloadViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = getLayoutInflater().inflate(R.layout.download_item, parent);
+            return new DownloadViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull DownloadViewHolder holder, int position) {
+            holder.binding(infos.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return this.infos.size();
+        }
+    }
+
+    private class DownloadViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        String fileName;
+        String DownloadURL;
+
+        TextView filenameView;
+        TextView URlview;
+        ImageButton startDownload;
+        ImageButton deleteView;
+
+        public DownloadViewHolder(@NonNull View itemView) {
+            super(itemView);
+            startDownload = itemView.findViewById(R.id.startDownload);
+            deleteView = itemView.findViewById(R.id.deleteDownloadinfo);
+            filenameView = itemView.findViewById(R.id.downloadTtitle);
+            URlview = itemView.findViewById(R.id.downloadUrl);
+
+            itemView.setOnClickListener(this);
+
+
+        }
+
+        @Override
+        public void onClick(View v) {
+switch (v.getId()){
+    case R.id.downloadTtitle:
+        break;
+}
+        }
+
+        public void binding(DownloadInfo info) {
+        }
+    }
 
 
 }
