791e518ac807c8eabc22ab2f796c81e9634ce3b6
Knightwood <kiylx@outlook.com>
2020-02-08 21:53:09 +0800

    emmm

--- NLauncher/app/src/main/java/com/crystal/android/nlauncher/NerdLauncherActivity.java
+++ NLauncher/app/src/main/java/com/crystal/android/nlauncher/NerdLauncherActivity.java
@@ -3,11 +3,11 @@ package com.crystal.android.nlauncher;
 
 import android.support.v4.app.Fragment;
 
-public class NerdLauncherActivity extends com.base.android.nerdlauncher.SingleFragmentActivity {
+public class NerdLauncherActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return com.base.android.nerdlauncher.NerdLauncherFeagment.newInstance();
+        return NerdLauncherFeagment.newInstance();
     }
 
 }
