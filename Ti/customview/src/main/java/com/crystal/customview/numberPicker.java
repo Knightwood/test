479d2fd8882246b67e72ddf51debc589cd3f0e1e
Knightwood <kiylx@outlook.com>
2020-04-03 16:21:10 +0800

    修bug

---
+++ Ti/customview/src/main/java/com/crystal/customview/numberPicker.java
@@ -0,0 +1,21 @@
+package com.crystal.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 创建者 kiylx
+ * 创建时间 2020/4/3 11:08
+ */
+public class numberPicker extends LinearLayout {
+    public numberPicker(Context context) {
+        super(context);
+    }
+
+    public numberPicker(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
